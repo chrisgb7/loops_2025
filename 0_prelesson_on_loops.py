1fc8a24c3ba918c54d959d9823d92ac62934cf86
chrisgb7 <gc6567824@gmail.com>
2025-12-09 20:48:09 +0000

    domer

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,29 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+#for loops = execute a block of code a fixed number of times 
+#               you can iterate over a range string sequence etc
+# for x in reversed(range (1, 11)): #counter weorks instead of x
+#     print(x)
+
+# print("happy new year")
+
+
+
+# for x in range (1, 11, 2):
+#     print(x)
+
+# credit_card = "12324-5678-9012-3456"
+# for x in credit_card:
+#     print(x)
+
+# for x in range (1, 21):
+#     if x == 12:
+#         continue
+#     else:
+#         print(x)
+
+for x in range (1, 21):
+    if x == 12:
+        break
+    else:
+        print(x)
--- 1_loops.py
+++ 1_loops.py
@@ -1,7 +1,7 @@
 # Example Practice:
 # Given this list of fruits:
 fruits = ["apple", "banana", "cherry", "date"]
-
+print(len(fruits))
 # Challenge:
 # Use a for loop to print each fruit on a new line.
 print(fruits[0])
@@ -15,13 +15,38 @@ for fruit in fruits:
 subjects = ["Math", "Science", "History", "Art"]
 for subject in subjects:
     print(subject)
+    print(len(subject))
+for subject in subjects:
+    print(subject)
+    if subject == "History":
+        continue
+    else:
+        print(subject)
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
-
+for subject in subjects:
+    print(subject)
+    if subject == "Science":
+        break
+    else:
+        print(subject)
 
 # Given:
 numbers = [5, 10, 15, 20]
 
 # Challenge:
 # Use a for loop to add all the numbers and print the total.
+for index in range(len(subjects)):
+    print("Subject"+ str(index) + ": " + subjects[index])
+
+total = 0 
+for number in numbers:
+    total += number
+print("total:", total)
+
+new_numbers = list(range(1, 600001))
+tote = 0
+for new_number in new_numbers:
+    tote += new_number
+print(tote)
